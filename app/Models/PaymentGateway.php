a1779dd5c789a00340b0f32ebbd13d5e2e86cc71
johnrecap <johnrecap@github.com>
2026-01-09 00:50:09 +0200

    Direct fix for payment images

--- app/Models/PaymentGateway.php
+++ app/Models/PaymentGateway.php
@@ -33,10 +33,19 @@ class PaymentGateway extends Model implements HasMedia
 
     public function getImageAttribute(): string
     {
+        // 1. First check for direct static file in public folder (Bypasses Storage/Symlink issues)
+        // We handle standard names: paymob.png, cashondelivery.png etc.
+        $staticPath = 'images/payment-gateways/' . str_replace('-', '', strtolower($this->slug)) . '.png';
+        if (file_exists(public_path($staticPath))) {
+            return asset($staticPath);
+        }
+
+        // 2. Then check Media Library
         $mediaUrl = $this->getFirstMediaUrl('payment-gateway');
         if (!empty($mediaUrl)) {
             return $mediaUrl;
         }
+
         return asset('images/default/payment-gateway/payment-gateway.png');
     }
 }
