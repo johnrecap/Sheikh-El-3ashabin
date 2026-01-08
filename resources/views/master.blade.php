e842fe433e22b67541b72531c0acfbf03ef5d080
johnrecap <johnrecap@github.com>
2026-01-08 23:00:04 +0200

    Add Facebook domain verification meta tag

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="facebook-domain-verification" content="oeiigzidn8l0zji7ize6hmuxqh9f33" />
 
     <!-- GOOGLE FONTS - POPPINS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -25,61 +26,70 @@
     <link rel="icon" type="image" href="{{ $favicon }}">
 
     @if (!blank($analytics))
-        @foreach ($analytics as $analytic)
-            @if (!blank($analytic->analyticSections))
-                @foreach ($analytic->analyticSections as $section)
-                    @if ($section->section == \App\Enums\AnalyticSection::HEAD)
-                        {!! $section->data !!}
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
+    @foreach ($analytics as $analytic)
+    @if (!blank($analytic->analyticSections))
+    @foreach ($analytic->analyticSections as $section)
+    @if ($section->section == \App\Enums\AnalyticSection::HEAD)
+    {!! $section->data !!}
+    @endif
+    @endforeach
+    @endif
+    @endforeach
     @endif
 
     <!-- Meta Pixel Code -->
     <script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1160701489564011');
-    fbq('track', 'PageView');
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1160701489564011');
+        fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=1160701489564011&ev=PageView&noscript=1"
-    /></noscript>
+            src="https://www.facebook.com/tr?id=1160701489564011&ev=PageView&noscript=1" /></noscript>
     <!-- End Meta Pixel Code -->
 </head>
 
 <body>
     @if (!blank($analytics))
-        @foreach ($analytics as $analytic)
-            @if (!blank($analytic->analyticSections))
-                @foreach ($analytic->analyticSections as $section)
-                    @if ($section->section == \App\Enums\AnalyticSection::BODY)
-                        {!! $section->data !!}
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
+    @foreach ($analytics as $analytic)
+    @if (!blank($analytic->analyticSections))
+    @foreach ($analytic->analyticSections as $section)
+    @if ($section->section == \App\Enums\AnalyticSection::BODY)
+    {!! $section->data !!}
+    @endif
+    @endforeach
+    @endif
+    @endforeach
     @endif
 
     <div id="app"></div>
 
     @if (!blank($analytics))
-        @foreach ($analytics as $analytic)
-            @if (!blank($analytic->analyticSections))
-                @foreach ($analytic->analyticSections as $section)
-                    @if ($section->section == \App\Enums\AnalyticSection::FOOTER)
-                        {!! $section->data !!}
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
+    @foreach ($analytics as $analytic)
+    @if (!blank($analytic->analyticSections))
+    @foreach ($analytic->analyticSections as $section)
+    @if ($section->section == \App\Enums\AnalyticSection::FOOTER)
+    {!! $section->data !!}
+    @endif
+    @endforeach
+    @endif
+    @endforeach
     @endif
 
     <script>
